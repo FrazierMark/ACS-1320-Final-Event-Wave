784d40436d77e131303e3791edf19f3ae71d8067
FrazierMark <m.mark.frazier@gmail.com>
2024-02-13 15:53:22 -0800

    Search bar done for now

--- src/Components/SearchBar/SearchBar.tsx
+++ src/Components/SearchBar/SearchBar.tsx
@@ -1,49 +1,61 @@
-
-import { useState } from "react";
-import { searchEvents } from "../../utils/searchApi.js";
+import { useState } from 'react';
+import { searchEvents } from '../../utils/searchApi.js';
 import { useNavigate } from 'react-router-dom';
 import { isValidZipCode } from '../../utils/helperFunctions.js';
-import './SearchBar.css'
-
+import './SearchBar.css';
 
 const SearchBar = () => {
-    const [zip, setZip] = useState("");
-    const [radius, setRadius] = useState("50");
-    const navigate = useNavigate();
+	const [zip, setZip] = useState('');
+	const [radius, setRadius] = useState('50');
+	const navigate = useNavigate();
 
-    const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const value = e.target.value;
-        setZip(value);
-    };
+	const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const value = e.target.value;
+		setZip(value);
+	};
 
-    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        if (isValidZipCode(zip)) {
-            const responseData = await searchEvents(zip, radius);
-            if (responseData) {
-                navigate("/events", { state: { search_results: responseData.data } });
-            }
-        }
-    };
+	const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		if (isValidZipCode(zip)) {
+			const responseData = await searchEvents(zip, radius);
+			if (responseData) {
+				navigate('/events', { state: { search_results: responseData.data } });
+			}
+		}
+	};
 
-    return (
-        <>
-            <form className="search-box" onSubmit={handleSubmit}>
-                <label className="search-label">Find Events Near You</label>
-                <input
-                    type="text"
-                    className="search-txt"
-                    placeholder="Zip Code"
-                    value={zip}
-                    onChange={onChange}
-                />
-                <button className="search-btn">
-                    <img className="img" src="images/search.png" alt="search icon" />
-                </button>
-            </form>
-            <div className="alert" id="zip-code-alert">Please enter a valid zip code.</div>
-        </>
-    );
+	return (
+		<>
+			<form className='search-bar' onSubmit={handleSubmit}>
+				<input
+					type='text'
+					className='search-query'
+					placeholder='Zip Code'
+					value={zip}
+					onChange={onChange}
+				/>
+				<button type='submit' className='search-btn'>
+					<svg
+						xmlns='http://www.w3.org/2000/svg'
+						fill='none'
+						viewBox='0 0 24 24'
+						strokeWidth='1.5'
+						stroke='#fff'
+						className='w-6 h-6'
+					>
+						<path
+							strokeLinecap='round'
+							strokeLinejoin='round'
+							d='M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z'
+						/>
+					</svg>
+				</button>
+			</form>
+			<div className='alert' id='zip-code-alert'>
+				Please enter a valid zip code.
+			</div>
+		</>
+	);
 };
 
 export default SearchBar;
--- src/utils/helperFunctions.tsx
+++ src/utils/helperFunctions.tsx
@@ -1,76 +1,76 @@
-
-
-export const  parseData = (responseData) => {
-    const eventsContext = [];
-    responseData.events.forEach((event) => {
-        const {
-            title,
-            id: seatgeek_id,
-            url,
-            datetime_utc: pub,
-            type: eventType,
-            performers,
-            venue,
-        } = event;
-        const performerArray = performers.map((performer) => performer.name);
-        const context = {
-            title,
-            seatgeek_id,
-            url,
-            pub,
-            performer: performers[0].name,
-            performers,
-            performerArray,
-            eventType,
-            image: performers[0].image,
-            venue: venue.name,
-        };
-        eventsContext.push(context);
-    });
-    return eventsContext
+export const parseData = (responseData) => {
+	const eventsContext = [];
+	responseData.events.forEach((event) => {
+		const {
+			title,
+			id: seatgeek_id,
+			url,
+			datetime_utc: pub,
+			type: eventType,
+			performers,
+			venue,
+		} = event;
+		const performerArray = performers.map((performer) => performer.name);
+		const context = {
+			title,
+			seatgeek_id,
+			url,
+			pub,
+			performer: performers[0].name,
+			performers,
+			performerArray,
+			eventType,
+			image: performers[0].image,
+			venue: venue.name,
+		};
+		eventsContext.push(context);
+	});
+	return eventsContext;
 };
 
-
 export const createGoogleEvent = (responseData) => {
-    const title = responseData['events'][0]['title']
-    const url = responseData['events'][0]['url']
-    const pub = responseData['events'][0]['datetime_utc']
-    const name_v2 = responseData.events[0].venue.name_v2.replace(/ /g, "+");
-    const address = responseData.events[0].venue.address.replace(/ /g, "+");
-    const extended_address = responseData.events[0].venue.extended_address.replace(/ /g, "+");
-    const googleEventTitle = title.replace(/ /g, "+");
-    const googleEventStart = pub.replace(/[^\w\s]|_/g, "").replace(/\s+/g, "");
-    const googleEventEnd = String(parseInt(googleEventStart.replace("T", "")) + 1);
-    const googleEventEndFormatted = googleEventEnd.slice(0, 8) + 'T' + googleEventEnd.slice(8);
-    const googleEventDetails = `&details=For+details,+link+here:+${url}`;
-    const googleEventAddress = `&location=${name_v2}${address}${extended_address}`;
+	const title = responseData['events'][0]['title'];
+	const url = responseData['events'][0]['url'];
+	const pub = responseData['events'][0]['datetime_utc'];
+	const name_v2 = responseData.events[0].venue.name_v2.replace(/ /g, '+');
+	const address = responseData.events[0].venue.address.replace(/ /g, '+');
+	const extended_address =
+		responseData.events[0].venue.extended_address.replace(/ /g, '+');
+	const googleEventTitle = title.replace(/ /g, '+');
+	const googleEventStart = pub.replace(/[^\w\s]|_/g, '').replace(/\s+/g, '');
+	const googleEventEnd = String(
+		parseInt(googleEventStart.replace('T', '')) + 1
+	);
+	const googleEventEndFormatted =
+		googleEventEnd.slice(0, 8) + 'T' + googleEventEnd.slice(8);
+	const googleEventDetails = `&details=For+details,+link+here:+${url}`;
+	const googleEventAddress = `&location=${name_v2}${address}${extended_address}`;
 
-    return `https://calendar.google.com/calendar/r/eventedit?text=${googleEventTitle}&dates=${googleEventStart}/${googleEventEndFormatted}${googleEventDetails}${googleEventAddress}`;
-}
-
+	return `https://calendar.google.com/calendar/r/eventedit?text=${googleEventTitle}&dates=${googleEventStart}/${googleEventEndFormatted}${googleEventDetails}${googleEventAddress}`;
+};
 
 export const formatDate = (dateString: string) => {
-    const date = new Date(dateString);
-    const options = {
-        weekday: 'long',
-        year: 'numeric',
-        month: 'long',
-        day: 'numeric',
-        hour: 'numeric',
-        minute: 'numeric',
-        hour12: true
-    };
-    return date.toLocaleString('en-US', options);
-}
+	const date = new Date(dateString);
+	const options = {
+		weekday: 'long',
+		year: 'numeric',
+		month: 'long',
+		day: 'numeric',
+		hour: 'numeric',
+		minute: 'numeric',
+		hour12: true,
+	};
+	return date.toLocaleString('en-US', options);
+};
 
-export const isValidZipCode = (zipCode) => {
-    const zipCodePattern = /^\d{5}(?:[-\s]\d{4})?$/;
-    const isValid = zipCodePattern.test(zipCode);
-    const alert = document.getElementById('zip-code-alert');
-    if (!isValid) {
-        alert.classList.add('invalid');
-    } else {
-        alert.classList.remove('invalid');
-    }
-    return isValid;
-}
+export const isValidZipCode = (zipCode: string) => {
+	const zipCodePattern = /^\d{5}(?:[-\s]\d{4})?$/;
+	const isValid = zipCodePattern.test(zipCode);
+	const alert = document.getElementById('zip-code-alert');
+	if (!isValid) {
+		alert!.classList.add('invalid');
+	} else {
+		alert!.classList.remove('invalid');
+	}
+	return isValid;
+};
