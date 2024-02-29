49c813326e073bbcf2df1ed01962778b1f9c73ec
FrazierMark <m.mark.frazier@gmail.com>
2024-02-29 10:25:59 -0800

    Deploy

--- src/Pages/LandingPage/LandingPage.tsx
+++ src/Pages/LandingPage/LandingPage.tsx
@@ -1,11 +1,7 @@
-import { useEffect, useMemo, useRef, useState } from 'react';
-import CanvasScene from '../../CanvasComponents/CanvasScene';
 import SearchBar from '../../Components/SearchBar/SearchBar';
-import SplitType from 'split-type';
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 gsap.registerPlugin(ScrollTrigger);
-import TagLine from '../../Components/TagLine/TagLine';
 import './LandingPage.css';
 
 const LandingPage = () => {
