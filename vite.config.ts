1ebf4e11a5aa95bd317a42aa3b4d12f262c13243
FrazierMark <m.mark.frazier@gmail.com>
2024-02-29 10:56:22 -0800

    Updated base

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import glsl from 'vite-plugin-glsl'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import glsl from 'vite-plugin-glsl';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), glsl()],
-  define: {
-    'process.env': {}
-  }
-})
+	base: '/ACS-1320-Final-Event-Wave/',
+	plugins: [react(), glsl()],
+	define: {
+		'process.env': {},
+	},
+});
