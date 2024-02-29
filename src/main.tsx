cb1cd26adde5027825da72aabeb930d02375b545
FrazierMark <m.mark.frazier@gmail.com>
2024-02-29 11:02:21 -0800

    Convert to hashrouter

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
-		<Router>
+		<HashRouter basename='/'>
 			<App />
-		</Router>
+		</HashRouter>
 	</React.StrictMode>
 );
